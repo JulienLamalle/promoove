aca32001cdccde2590f9ab8a426e4a2645ee4071
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-14 04:02:29 +0100

    added index: true on belongs_to into migrations

--- db/migrate/20210314014845_create_project_languages.rb
+++ db/migrate/20210314014845_create_project_languages.rb
@@ -1,8 +1,8 @@
 class CreateProjectLanguages < ActiveRecord::Migration[5.2]
   def change
     create_table :project_languages do |t|
-      t.belongs_to :language
-      t.belongs_to :project
+      t.belongs_to :language, index: true
+      t.belongs_to :project, index: true
       t.timestamps
     end
   end
