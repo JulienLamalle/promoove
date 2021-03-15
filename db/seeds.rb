662bed1ad284196f61e161f6a6986b104a0a9f08
AmauryDBZ <amaury.desbazeille@gmail.com>
2021-03-15 23:06:44 +0100

    seed in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faker'
+Faker::Config.locale = 'fr'
+
 User.destroy_all
 User.reset_pk_sequence
 Project.destroy_all
@@ -43,26 +46,150 @@ CommentAnswerUpvote.reset_pk_sequence
 CommentUpvote.destroy_all
 CommentUpvote.reset_pk_sequence
 
-user = User.create(email: "julien@lamalle.com", password: "azerty", first_name: "Julien", last_name: "Lamalle", description: "Coucou", date_of_birth: "2021-03-13", professional_background: "THP", github_link: "mon super lien", gitlab_link: "mon super lien", twitter_link: "mon super lien", linkedin_link: "mon super lien")
-project = Project.create(name: "Google", short_description: "I am a short description", long_description: "I am a long description", value_of_project: 1000, money_earned: 100, number_of_developpers_on_project: 3, daily_time_spent_on_project_per_developper: 4, is_complete: true, link_of_github: "iamthelink", license: "license123", is_validated: true)
-role = Role.create(name: "CTO")
-collab = Collaboration.create(user: user, project: project, role: role)
-donation = Donation.create(user: user, project: project, amount: 2000)
-media_type = MediaType.create(name: 'image')
-media = Media.create(media_type: MediaType.first, aws_link: 'https://www.link.fr')
-project_media = ProjectMedia.create(project_id: 1, media_id: 1)
-category = Category.create(name: 'Intelligence artificielle')
-project_category = ProjectCategory.create(category: Category.last, project: Project.last)
-language_media = LanguageMedia.create(data_type: 'image', aws_link: 'www.hello.com')
-language = Language.create(name: 'Ruby', language_media: LanguageMedia.last)
-project_language = ProjectLanguage.create(project: Project.last, language: language)
-competition = Competition.create(name: 'Best competition', duration: 7)
-project_competition = ProjectCompetition.create(project: project, competition: competition, upvote_number: 132)
-project_competition_upvote = ProjectCompetitionUpvote.create(project_competition: project_competition, user: user )
-badge_type = BadgeType.create(name: 'Or')
-badge_media = BadgeMedia.create(aws_link: 'linkdunbadge.com')
-badge = Badge.create(badge_type: BadgeType.last, badge_media: BadgeMedia.last, project_competition: project_competition)
-comment = Comment.create(content: "Ceci est un commentaire comme on en fait plus", user: user, project: project)
-comment_answer = CommentAnswer.create(content: "Ceci est un commentaire d'un commentaire comme on en fait plus", user: user, comment: comment)
-comment_answer_upvote = CommentAnswerUpvote.create(user: user, comment_answer: comment_answer)
-comment_upvote = CommentUpvote.create(user: user, comment: comment)
+# Arrays of data
+media_type = ["text", "image", "videos", "son"]
+category = ["Intelligence Artificielle", "BTP", "Santé", "FinTech", "Sport"]
+languages = ["python", "javascript", "ruby", "C#", "HTML"]
+
+# Users
+10.times do
+    user = User.create(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password,
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        description: Faker::Books::Dune.quote,
+        date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65),
+        professional_background: Faker::Job.field
+    )
+    user.update(
+        email: "#{user.first_name}.#{user.last_name}@yopmail.com",
+        github_link: "github.io/#{user.first_name}_#{user.last_name}",
+        gitlab_link: "gitlab.io/#{user.first_name}_#{user.last_name}",
+        twitter_link: "twitter.com/#{user.first_name}_#{user.last_name}",
+        linkedin_link: "linkedin.com/#{user.first_name}_#{user.last_name}"
+    )
+    puts "seeding user #{user.first_name}"
+end
+
+# Projects
+5.times do
+    project = Project.create(
+        name: Faker::Beer.name,
+        short_description: Faker::Books::Dune.quote,
+        long_description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 4),
+        value_of_project: Faker::Number.number(digits: 4),
+        money_earned: Faker::Number.number(digits: 3),
+        number_of_developpers_on_project: Faker::Number.number(digits: 2),
+        daily_time_spent_on_project_per_developper: Faker::Number.number(digits: 1),
+        is_complete: Faker::Boolean.boolean,
+        license: Faker::Code.rut,
+        is_validated: Faker::Boolean.boolean
+    )
+    project.update(link_of_github: "github.io/#{project.name}")
+    puts "seeding project #{project.name}"
+end
+
+# Roles
+10.times do
+    role = Role.create(name: Faker::Job.seniority)
+    puts "seeding role #{role.name}"
+end
+
+# Collaborations
+20.times do
+    collab = Collaboration.create(user: User.all.sample,
+    project: Project.all.sample,
+    role: Role.all.sample
+    )
+end
+puts "collaborations created"
+
+# Donations
+25.times do
+    donation = Donation.create(user: User.all.sample,
+    project: Project.all.sample,
+    amount: Faker::Number.number(digits: 2)
+    )
+end
+puts "donations dones"
+
+# Media type
+i = 0
+media_type.size.times do
+    MediaType.create(name: media_type[i])
+    i += 1
+end
+puts "media types created"
+
+# Media
+25.times do
+    media = Media.create(media_type: MediaType.first,
+    aws_link: Faker::Internet.url(host: 'aws.com')
+    )
+end
+puts "Media created"
+
+# Project Media
+25.times do
+    project_media = ProjectMedia.create(project_id: 1, media_id: 1)
+end
+puts "Project media created"
+
+# Category
+i = 0
+category.size.times do
+    Category.create(name: category[i])
+    i += 1
+end
+puts "category created"
+
+# Project category
+15.times do
+    project_category = ProjectCategory.create(category: Category.all.sample,
+    project: Project.all.sample
+    )
+end
+puts "project category"
+
+# Language Media
+5.times do
+    language_media = LanguageMedia.create(data_type: 'image',
+    aws_link: Faker::Internet.url(host: 'aws.com')
+    )
+end
+puts "language media"
+
+# Language
+i = 0
+languages.size.times do
+    language = Language.create(name: languages[i],
+    language_media: LanguageMedia.last
+    )
+    i += 1
+end
+puts "languages"
+
+#Project Language
+3.times do
+    project_language = ProjectLanguage.create(project: Project.all.sample, language: Language.all.sample)
+end
+puts "project languages"
+
+# Competition
+3.times do
+    competition = Competition.create(name: Faker::Team.state + ' league', duration: 7)
+end
+puts "competition"
+
+=begin
+    project_competition = ProjectCompetition.create(project: project, competition: competition, upvote_number: 132)
+    project_competition_upvote = ProjectCompetitionUpvote.create(project_competition: project_competition, user: user )
+    badge_type = BadgeType.create(name: 'Or')
+    badge_media = BadgeMedia.create(aws_link: 'linkdunbadge.com')
+    badge = Badge.create(badge_type: BadgeType.last, badge_media: BadgeMedia.last, project_competition: project_competition)
+    comment = Comment.create(content: "Ceci est un commentaire comme on en fait plus", user: user, project: project)
+    comment_answer = CommentAnswer.create(content: "Ceci est un commentaire d'un commentaire comme on en fait plus", user: user, comment: comment)
+    comment_answer_upvote = CommentAnswerUpvote.create(user: user, comment_answer: comment_answer)
+    comment_upvote = CommentUpvote.create(user: user, comment: comment)
+=end
