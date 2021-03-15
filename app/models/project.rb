29a5fc494bac95ca079c33997f4401fa5369562a
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-15 21:40:26 +0100

    added slug to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,19 @@
 class Project < ApplicationRecord
-    has_many :collaborations
-    has_many :users, through: :collaborations
-    has_many :donations
-    has_many :project_medias
-    has_many :medias, through: :project_medias
-    has_many :project_competitions
-    has_many :competitions, through: :project_competitions
-    has_many :comments
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+	has_many :collaborations
+  has_many :users, through: :collaborations
+  has_many :donations
+  has_many :project_medias
+  has_many :medias, through: :project_medias
+	has_many :project_competitions
+  has_many :competitions, through: :project_competitions
+  has_many :comments
+
+  private 
+
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_174843) do
+ActiveRecord::Schema.define(version: 2021_03_15_201554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,17 @@ ActiveRecord::Schema.define(version: 2021_03_15_174843) do
     t.index ["user_id"], name: "index_donations_on_user_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "language_media", force: :cascade do |t|
     t.string "data_type"
     t.string "aws_link"
@@ -225,6 +236,8 @@ ActiveRecord::Schema.define(version: 2021_03_15_174843) do
     t.boolean "is_validated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_projects_on_slug", unique: true
   end
 
   create_table "roles", force: :cascade do |t|
