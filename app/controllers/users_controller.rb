d338a5cfec1aa35cd5b12c5d21da6381fe081282
AmauryDBZ <amaury.desbazeille@gmail.com>
2021-03-17 20:46:51 +0100

    card format ok, links not available

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    #@project_competition_upvote = ProjectCompetitionUpvote.where(user: @user)
+    @projects = Project.all
+    #@categories = ProjectCategory.where(project_id: )
+    #@project_competitions = ProjectCompetition.find()
   end
 end
