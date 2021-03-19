55b4216e098d74e1b2d12924d59e32e2f8678035
AmauryDBZ <amaury.desbazeille@gmail.com>
2021-03-19 18:14:23 +0100

    problem with hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @project_collaborations = Collaboration.includes(:project).where(user: @user).all
-    @upvotes = ProjectUpvote.includes(:project)
+    @project_collaborations = Collaboration.includes(:project).where(user: @user)
+
+    @all_collaboration_projects = Array.new
+    @project_collaborations.each do |collaboration| 
+      @project_data = Hash.new
+      @project_data[:testeur] = "works"
+      @project_data[:project] = collaboration.project
+      @project_data[:role] = collaboration.role
+      @project_data[:upvotes] = ProjectUpvote.where(project: collaboration.project).size
+      @project_data[:categories] = ProjectCategory.includes(:category).where(project: collaboration.project)
+      @all_collaboration_projects << @project_data
+    end
+    puts "------------------------------"
+    pp @all_collaboration_projects << @project_data
+    puts "*****************************"
   end
 end
