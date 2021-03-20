17d56f0d173dab11a302a3fdcca1fa8d5fedf64c
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-20 16:37:24 +0100

    welcome_mailer done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   config.middleware.use(Rack::LiveReload)
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  
   config.after_initialize do
     Bullet.enable = true # active Bullet
     Bullet.bullet_logger = true # génère des logs dans log/bullet.log
