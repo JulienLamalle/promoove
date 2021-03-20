e375693c0727a097a131c8bfaa1f029af2d9a53b
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-20 06:25:46 +0100

    changed hash creation in users controller, display user informations on user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @project_collaborations = Collaboration.includes(:project).where(user: @user)
+    @project_collaborations = Collaboration.includes(:project, :role).where(user: @user)
 
     @all_collaboration_projects = Array.new
     @project_collaborations.each do |collaboration| 
-      @project_data = Hash.new
-      @project_data[:testeur] = "works"
-      @project_data[:project] = collaboration.project
-      @project_data[:role] = collaboration.role
-      @project_data[:upvotes] = ProjectUpvote.where(project: collaboration.project).size
-      @project_data[:categories] = ProjectCategory.includes(:category).where(project: collaboration.project)
+      @project_data = {
+        testeur: "works",
+        project: collaboration.project,
+        role: collaboration.role,
+        upvotes: ProjectUpvote.where(project: collaboration.project).size,
+        categories: ProjectCategory.includes(:category).where(project: collaboration.project)
+      }
       @all_collaboration_projects << @project_data
     end
-    puts "------------------------------"
-    pp @all_collaboration_projects << @project_data
-    puts "*****************************"
   end
 end
