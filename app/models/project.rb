3b927edeb0835160794804a1e0440bacdf215605
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-21 17:09:55 +0100

    show category view done

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,10 +5,10 @@ class Project < ApplicationRecord
 	has_many :collaborations
   has_many :users, through: :collaborations
   has_many :donations
-  has_many :project_medias
+  has_many :project_medias 
   has_many :medias, through: :project_medias
-  has_many :comments
-  has_many :project_upvotes
+  has_many :comments, dependent: :destroy
+  has_many :project_upvotes, dependent: :destroy
 
   validates :name, presence: true, length: {minimum: 3}, uniqueness: true
   validates :short_description, length: {maximum: 500, too_long: "La description courte doit comporter %{count} caractères maximum"}
