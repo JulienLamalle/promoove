a36a1c1124e43f1cd56b7314c1b91a002a7f0d94
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-23 06:48:25 +0100

    updated language model with slug verification in case how name changed

--- app/models/language.rb
+++ app/models/language.rb
@@ -8,4 +8,10 @@ class Language < ApplicationRecord
   belongs_to :language_media
 
   validates :name, presence: true
+
+  private 
+
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
 end
