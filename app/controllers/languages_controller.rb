09a2cc09694b43bd59fc89ebd8618886564a814a
JulienLamalle <julienlamalle63100@gmail.com>
2021-03-23 08:03:41 +0100

    show language view done, updated database: deletion of language_media table, added picture_url link directly on languages_table, updated seed

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -5,5 +5,7 @@ class LanguagesController < ApplicationController
 
   def show
     @language = Language.friendly.find(params[:id])
+    @languages = Language.where.not(id: @language.id).order("RANDOM()").take(5)
+    @projects = Project.joins(:project_languages).where(project_languages: {language_id: @language.id}).order("RANDOM()").take(24)
   end
 end
--- db/migrate/20210314014643_create_languages.rb
+++ db/migrate/20210314014643_create_languages.rb
@@ -2,7 +2,6 @@ class CreateLanguages < ActiveRecord::Migration[5.2]
   def change
     create_table :languages do |t|
       t.string :name
-      t.belongs_to :language_media, index: true
       t.timestamps
     end
   end
