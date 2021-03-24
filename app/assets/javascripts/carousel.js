4d662f3483580895871c9fb790ac46accaf6f38d
JulietDeRozario <juliet.dr@tutanota.com>
2021-03-24 11:01:38 +0100

    js mooved into functions

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,13 +1,14 @@
-let modal = document.getElementById("myModal");
-let modalImg = document.getElementById("modal-img");
-let captionText = document.querySelector("#caption");
-document.querySelectorAll(".myImg").forEach(img => {
-  img.addEventListener('click', event => {
-    modal.style.display = "block";
-    modalImg.src = event.target.src;
+const openImage = () => {
+  let modal = document.getElementById("myModal");
+  let modalImg = document.getElementById("modal-img");
+  document.querySelectorAll(".myImg").forEach(img => {
+    img.addEventListener('click', event => {
+      modal.style.display = "block";
+      modalImg.src = event.target.src;
+    })
   })
-})
-let span = document.querySelector(".close");
-span.onclick = function() {
-  modal.style.display = "none";
+  let span = document.querySelector(".close");
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 }
--- app/assets/javascripts/comment_answer.js
+++ app/assets/javascripts/comment_answer.js
@@ -1,10 +1,11 @@
-console.log("Je suis dans dans comment_ansx=wer.js bitches !!!!!!!");
-document.querySelectorAll(".display_answers").forEach(button => {
-  button.addEventListener('click', event => {
-    if(document.getElementById(`answers-${button.id}`).style.display == "none"){
-      document.getElementById(`answers-${button.id}`).style.display = "block";
-    }else if(document.getElementById(`answers-${button.id}`).style.display == "block"){
-      document.getElementById(`answers-${button.id}`).style.display = "none";
-    }
+const displayAnswers = () => {
+  document.querySelectorAll(".display_answers").forEach(button => {
+    button.addEventListener('click', event => {
+      if(document.getElementById(`answers-${button.id}`).style.display == "none"){
+        document.getElementById(`answers-${button.id}`).style.display = "block";
+      }else if(document.getElementById(`answers-${button.id}`).style.display == "block"){
+        document.getElementById(`answers-${button.id}`).style.display = "none";
+      }
+    });
   });
-});
+}
