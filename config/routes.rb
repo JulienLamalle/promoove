e61a47bafcdc7888954ead8ba542d520d2c3960d
Benoit Ferretti <benoit.ferretti@edu.em-lyon.com>
2021-03-24 20:00:08 +0100

    changed name from static_pages to about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'about/index'
   get 'static_pages/about'
   root 'projects#index'
   devise_for :users, skip: [:session, :password, :registration], :controllers => { omniauth_callbacks: "callbacks"}
@@ -11,9 +12,9 @@ Rails.application.routes.draw do
     resources :users, only: [:show]
     resources :comments
     resources :comment_answers
+    resources :about, only: [:index]
     resources :languages, only: [:index, :show]
     resources :admin, only: [:index]
-    resources :static_pages, only: [:index]
     namespace :admin do 
       resources :projects, only: [:index]
       resources :users, except: [:show, :create]
