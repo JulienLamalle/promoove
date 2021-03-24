ced1cafb6ad491086352b12e937924446ef57279
JulietDeRozario <juliet.dr@tutanota.com>
2021-03-24 10:54:24 +0100

    require edition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,5 +47,3 @@
 //= require tooltip.js
 //= require quick-website.js
 //= require orders.js
-//= require carousel.js
-//= require comment_answer.js
--- app/assets/javascripts/toast.js
+++ app/assets/javascripts/toast.js
@@ -2,3 +2,5 @@
 //= require toastr.min.js
 //= require toastrparams.js
 //= require typed.min.js
+//= require comment_answer.js
+//= require carousel.js
