b6a151db9af5c9385239564a1eaec73dac47f3ac
JulietDeRozario <juliet.dr@tutanota.com>
2021-03-31 10:22:54 +0200

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_072532) do
+ActiveRecord::Schema.define(version: 2021_03_29_085001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2021_03_27_072532) do
     t.bigint "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "upvotes_count", default: 0
     t.index ["comment_id"], name: "index_comment_answers_on_comment_id"
     t.index ["user_id"], name: "index_comment_answers_on_user_id"
   end
@@ -139,6 +140,7 @@ ActiveRecord::Schema.define(version: 2021_03_27_072532) do
     t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "upvotes_count", default: 0
     t.index ["project_id"], name: "index_comments_on_project_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
