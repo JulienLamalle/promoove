1cec9b72e69b6c0e90efac308ce272442c44ae82
JulietDeRozario <juliet.dr@tutanota.com>
2021-03-31 10:34:38 +0200

    migration removed

--- db/migrate/20210329085001_add_upvotes_count_to_comments_and_comment_answers.rb
+++
@@ -1,6 +0,0 @@
-class AddUpvotesCountToCommentsAndCommentAnswers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :comments, :upvotes_count, :integer, default: 0
-    add_column :comment_answers, :upvotes_count, :integer, default: 0
-  end
-end
